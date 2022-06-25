f30223ed6ea9aaf4adef9d8cc9fa9de591fd601d
Harshit Singhal <41239446+harshit0410@users.noreply.github.com>
2022-06-25 14:20:08 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,79 @@
+var data = {
+  "name": "Harshit Singhal",
+  "title": "Software Engineer at Postman",
+  "location": "Bengaluru, Karnataka, India",
+  "about": "A dynamic personality with the ability of problem solving and seeking for innovation. Passionate to work in the field of software development.",
+  "profilePicture": "https://harshit0410.github.io/profilePicture.jpeg",
+  "socialProfiles": [
+    {
+      "name": "LinkedIn",
+      "link": "https://in.linkedin.com/in/harshitsinghal0410"
+    },
+    {
+      "name": "Github",
+      "link": "https://github.com/harshit0410"
+    }
+  ],
+  "experience": [
+    {
+      "name": "Postman",
+      "title": "Software Engineer"
+    },
+    {
+      "name": "ONGC",
+      "title": "Summer Intern"
+    }
+  ],
+  "education": [
+    {
+      "name": "DIT University",
+      "course": "Bachelor of Technology in Computer Science Engineering",
+      "yearOfPassing": "2021",
+      "grade": "9.24 CGPA"
+    },
+    {
+      "name": "Children's Academy",
+      "course": "Class XII",
+      "yearOfPassing": "2017",
+      "grade": "95.2 %"
+    },
+    {
+      "name": "Children's Academy",
+      "course": "Class X",
+      "yearOfPassing": "2015",
+      "grade": "9.8 CGPA"
+    }
+  ],
+  "certifications": [
+    {
+      "name": "AWS Certified Cloud Practitioner",
+      "organisation": "Amazon Web Services",
+      "issueDate": "Jan 2022"
+    },
+    {
+      "name": "Docker Basics",
+      "organisation": "Learn Code Online",
+      "issueDate": "Aug 2021"
+    },
+    {
+      "name": "Full Stack MERN Bootcamp",
+      "organisation": "Learn Code Online",
+      "issueDate": "Dec 2020"
+    },
+    {
+      "name": "Business Analytics",
+      "organisation": "Upgrad",
+      "issueDate": "May 2020"
+    },
+    {
+      "name": "IBM Data Science Professional Certificate",
+      "organisation": "Coursera",
+      "issueDate": "Nov 2019"
+    },
+    {
+      "name": "Web Applications for Everybody Specialization",
+      "organisation": "Coursera",
+      "issueDate": "Nov 2019"
+    }
+  ]
+};
